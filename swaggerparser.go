b4193de04870288db091c8b97672061862cfd785
nharis <nharis@cisco.com>
2017-05-04 21:20:00 -0700

    first commit

---
+++ swaggerparser.go
@@ -0,0 +1,123 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+
+	"encoding/json"
+	"github.com/buger/jsonparser"
+	"strings"
+	"YJparser/yamlparser"
+)
+
+
+type Swagger struct{
+	Swagger string   `json:"swagger"`
+	Paths json.RawMessage
+	//Paths  map[string]interface{} `json:"paths"`
+	//Definitions  map[string]interface{}  `json:"definitions"`
+	Definitions json.RawMessage
+}
+
+
+type Definitionsprops struct {
+	name string
+	Properties map[string]interface{} `json:"properties"`
+	indprop []property
+	//ppty []*property
+
+}
+
+type property struct {
+	Name string
+	Type string
+	Format string
+	Items interface{}
+	Enum interface{}
+	Default bool
+
+}
+func main() {
+	filePath := "./swagger_webapp.json";
+	fmt.Printf( "// reading file %s\n", filePath )
+	file, err1 := ioutil.ReadFile( filePath )
+
+	// parsing Yaml to populate structures
+	if err1 != nil {
+		fmt.Printf( "// error while reading file %s\n", filePath )
+		fmt.Printf("File error: %v\n", err1)
+		os.Exit(1)
+	}
+	var swag *Swagger
+	err2 := json.Unmarshal(file,&swag)
+	fmt.Printf("this is swag value : %s \n",swag.Swagger)
+	fmt.Println("================================")
+	meta := yamlparser.Model{}
+	yamlparser.ParseYaml(&meta)
+	fmt.Println(meta)
+	for i := range meta.Relationships {
+		fmt.Println(string(meta.Relationships[i].Name))
+
+		v, _, _, _ := jsonparser.Get(swag.Paths, "/" + meta.Relationships[i].Name, "get", "responses", "200", "schema", "$ref")
+		fmt.Printf("%s\n", string(v))
+		fmt.Println("================================")
+		defintion := strings.SplitAfter(string(v), "#/definitions/")
+		fmt.Println(defintion[1])
+		//fmt.Printf( "The defintion %s \n", (swag.Definitions));
+		def, _, _, _ := jsonparser.Get(swag.Definitions, defintion[1])
+		//fmt.Println(string(def))
+		var vardef Definitionsprops
+		_ = json.Unmarshal(def, &vardef)
+		vardef.name = defintion[1]
+		fmt.Println(vardef)
+		//v,_,_,_ = jsonparser.Get(swag.Definitions,defintion[1],"properties")
+		fmt.Println("================================")
+
+		for key, val := range vardef.Properties {
+			lname := key
+			ltype := val.(map[string]interface{})["type"]
+			if ltype == nil {
+				ltype = ""
+			}
+
+			lFormat := val.(map[string]interface{})["format"]
+			if lFormat == nil {
+				lFormat = ""
+			}
+			lItems := val.(map[string]interface{})["items"]
+			if lItems == nil {
+				lItems = ""
+			}
+			lEnum := val.(map[string]interface{})["enum"]
+			if lEnum == nil {
+				lEnum = ""
+			}
+			lDefault := val.(map[string]interface{})["default"]
+			if lDefault == nil {
+				lDefault = false
+			}
+			fmt.Println("================================")
+			fmt.Println("new property")
+			fmt.Println("Name:", lname)
+			fmt.Println("Type:", ltype)
+			fmt.Println("Format:", lFormat)
+			fmt.Println("Items:", lItems)
+			fmt.Println("Enum:", lEnum)
+			fmt.Println("Default:", lDefault)
+			fmt.Println("================================")
+
+			tmpProperty := property{Name:string(lname), Type:ltype.(string), Format:lFormat.(string), Items:lItems, Enum:lEnum, Default:lDefault.(bool)}
+
+			vardef.indprop = append(vardef.indprop, tmpProperty)
+
+		}
+	}
+	//var propertymap map[string]json.RawMessage
+
+	fmt.Print(err2)
+
+
+
+}
+
---
+++ yamlparser/yamlparser.go
@@ -0,0 +1,41 @@
+package yamlparser
+
+import (
+	"fmt"
+	"os"
+	"gopkg.in/yaml.v2"
+	"io/ioutil"
+)
+type Model struct{
+	Source_dir string
+	Relationships	[]Relations
+}
+
+type Relations struct {
+	Name string
+}
+
+func ParseYaml(mod *Model) {
+	filePath := "./hx.yaml";
+	fmt.Printf( "// reading file %s\n", filePath )
+	file, err1 := ioutil.ReadFile( filePath )
+	if err1 != nil {
+		fmt.Printf( "// error while reading file %s\n", filePath )
+		fmt.Printf("File error: %v\n", err1)
+		os.Exit(1)
+	}
+
+	//var mod *Model
+
+	err2 := yaml.Unmarshal(file, &mod)
+	if err2 != nil {
+		fmt.Println("error:", err2)
+		os.Exit(1)
+	}
+
+	fmt.Println( "// loop over array of structs of Relations" )
+	fmt.Printf( "The Model '%s\n", mod.Source_dir  );
+	for k := range mod.Relationships {
+		fmt.Printf( "The name %s \n", mod.Relationships[k].Name);
+	}
+}
