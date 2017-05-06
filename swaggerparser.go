c5815582af460a0b3dd7962b63cac02c0391391b
nharis <nharis@cisco.com>
2017-05-06 14:40:21 -0700

    sample model generated 5/6 2.40pm

--- swaggerparser.go
+++ swaggerparser.go
@@ -9,20 +9,24 @@ import (
 	"github.com/buger/jsonparser"
 	"strings"
 	"YJparser/yamlparser"
+	"text/template"
+
 )
 
 
+
 type Swagger struct{
-	Swagger string   `json:"swagger"`
+	SwagVersion string   `json:"swagger"`
 	Paths json.RawMessage
 	//Paths  map[string]interface{} `json:"paths"`
 	//Definitions  map[string]interface{}  `json:"definitions"`
 	Definitions json.RawMessage
+	Defs []*Definitionsprops
 }
-
-var Defs []Definitionsprops
+var Swag Swagger
 type Definitionsprops struct {
-	name string
+	Name string
+	Type string
 	Properties map[string]interface{} `json:"properties"`
 	//Properties map[string]json.RawMessage  `json:"properties"`
 	indprop []property
@@ -40,6 +44,173 @@ type property struct {
 	Default bool		`json:"default"`
 
 }
+
+func ParseDefintions( aInDefintionName string, jsonRawDef []byte){
+
+	var vardef Definitionsprops
+	_ = json.Unmarshal(jsonRawDef, &vardef)
+	vardef.Name = aInDefintionName
+	vardef.Type = "object"
+	fmt.Println(vardef)
+	//v,_,_,_ = jsonparser.Get(swag.Definitions,defintion[1],"properties")
+	fmt.Println("================================")
+
+	for key, val := range vardef.Properties {
+		lname := key
+		/*var tmpProp property
+		_ = json.Unmarshal(val, &tmpProp )
+		tmpProp.Name = lname
+		fmt.Println("================================")
+
+		vardef.indprop = append(vardef.indprop, tmpProp)
+		fmt.Println("Name:",tmpProp.Name)
+		fmt.Println("Type:",tmpProp.Type)
+		fmt.Println("Format:",tmpProp.Format)
+		fmt.Println("Enum:",tmpProp.Enum)
+		fmt.Println("Items:",tmpProp.Items)
+		fmt.Println("Default:",tmpProp.Default)
+
+		fmt.Println("================================")*/
+
+
+		ltype := val.(map[string]interface{})["type"]
+		if ltype == nil {
+			ltype = ""
+		}
+
+		lFormat := val.(map[string]interface{})["format"]
+		if lFormat == nil {
+			lFormat = ""
+		}
+		lItems := val.(map[string]interface{})["items"]
+
+		if lItems != nil {
+			fmt.Println("lItems:",lItems)
+			for keyItem,valItem := range (lItems).(map[string]interface{}) {
+
+				if keyItem == "$ref"{
+					defintion := strings.SplitAfter((valItem.(string)), "#/definitions/")
+					fmt.Println(defintion[1])
+					//fmt.Printf( "The defintion %s \n", (swag.Definitions));
+					def, _, _, _ := jsonparser.Get(Swag.Definitions, defintion[1])
+					fmt.Println("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxTHISISCHILDxxxxxxxxxxxxxxxxxxxxxxxxxxx")
+
+					ParseDefintions(defintion[1],def)
+					fmt.Println("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxCHILDENDSHERExxxxxxxxxxxxxxxxxxxxxxxxxxx")
+
+				}
+			}
+		}else{
+			lItems = ""
+		}
+		lEnum := val.(map[string]interface{})["enum"]
+		if lEnum == nil {
+			lEnum = ""
+		}
+		lDefault := val.(map[string]interface{})["default"]
+		if lDefault == nil {
+			lDefault = false
+		}
+		lRefs := val.(map[string]interface{})["$ref"]
+		if lRefs != nil {
+			defintion := strings.SplitAfter((lRefs).(string), "#/definitions/")
+			fmt.Println(defintion[1])
+			//fmt.Printf( "The defintion %s \n", (swag.Definitions));
+			def, _, _, _ := jsonparser.Get(Swag.Definitions, defintion[1])
+			fmt.Println("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxTHISISCHILDxxxxxxxxxxxxxxxxxxxxxxxxxxx")
+
+			ParseDefintions(defintion[1],def)
+			fmt.Println("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxCHILDENDSHERExxxxxxxxxxxxxxxxxxxxxxxxxxx")
+
+		}else{
+			lRefs = ""
+		}
+		fmt.Println("================================")
+		fmt.Println("new property")
+		fmt.Println("Name:", lname)
+		fmt.Println("Type:", ltype)
+		fmt.Println("Format:", lFormat)
+		fmt.Println("Items:", lItems)
+		fmt.Println("Enum:", lEnum)
+		fmt.Println("Refs:", lRefs)
+		fmt.Println("Default:", lDefault)
+		fmt.Println("================================")
+
+		tmpProperty := property{Name:string(lname), Type:ltype.(string), Format:lFormat.(string), Items:lItems, Enum:lEnum,Refs:lRefs.(string), Default:lDefault.(bool)}
+
+		vardef.indprop = append(vardef.indprop, tmpProperty)
+
+	}
+	Swag.Defs = append(Swag.Defs,&vardef)
+	fmt.Println("Function ends here")
+}
+
+const (
+	dirFilePerm os.FileMode = 0777
+
+	modelDirectory string = "model"
+	metaDirectory  string = "meta/"
+	projectKey     string = "bitbucket-eng-sjc1.cisco.com/an"
+)
+
+
+func loadTemplate(name string) *template.Template {
+	gopath := os.Getenv("GOPATH")
+	if gopath == "" {
+		fmt.Print("Environment variable 'GOPATH' must be set.")
+	}
+
+	fname := name
+	fmt.Printf("Parsing template '%s'", fname)
+	t, err := template.ParseFiles(fname)
+	if err != nil {
+		fmt.Print("Invalid adgen template : ", err)
+	}
+	return t
+}
+
+func createDir(name string) {
+	if _, err := os.Stat(name); os.IsNotExist(err) {
+		err := os.MkdirAll(name, dirFilePerm)
+		if err != nil {
+			fmt.Printf("cannot make dir '%s' : %s", name, err)
+		}
+	}
+}
+
+func createFile(name string) *os.File {
+
+	f, err := os.OpenFile(name, os.O_CREATE|os.O_TRUNC|os.O_WRONLY, dirFilePerm)
+	if err != nil {
+		fmt.Printf("Failed to create file '%s' : %s", name, err.Error())
+	}
+
+	return f
+}
+
+func generateModel(mg Swagger){
+	var t *template.Template
+	var f *os.File
+	fmt.Print("Generating Model File")
+	t = loadTemplate("Model.gotmpl")
+
+	createDir("GenModel")
+
+	f = createFile("GenModel/ModelGen.yaml")
+	defer func(f *os.File) {
+		if err := f.Close(); err != nil {
+			fmt.Print("Failed to close file: ", err)
+		}
+	}(f)
+
+	err := t.Execute(f, mg)
+	if err != nil {
+		fmt.Print("Error processing template")
+	}
+
+
+}
+
 func main() {
 	filePath := "./swagger_webapp.json";
 	fmt.Printf( "// reading file %s\n", filePath )
@@ -51,9 +222,9 @@ func main() {
 		fmt.Printf("File error: %v\n", err1)
 		os.Exit(1)
 	}
-	var swag *Swagger
-	err2 := json.Unmarshal(file,&swag)
-	fmt.Printf("this is swag value : %s \n",swag.Swagger)
+	//var swag *Swagger
+	err2 := json.Unmarshal(file,&Swag)
+	fmt.Printf("this is swag value : %s \n",Swag.SwagVersion)
 	fmt.Println("================================")
 	meta := yamlparser.Model{}
 	yamlparser.ParseYaml(&meta)
@@ -61,81 +232,17 @@ func main() {
 	for i := range meta.Relationships {
 		fmt.Println(string(meta.Relationships[i].Name))
 
-		v, _, _, _ := jsonparser.Get(swag.Paths, "/" + meta.Relationships[i].Name, "get", "responses", "200", "schema", "$ref")
+		v, _, _, _ := jsonparser.Get(Swag.Paths, "/" + meta.Relationships[i].Name, "get", "responses", "200", "schema", "$ref")
 		fmt.Printf("%s\n", string(v))
 		fmt.Println("================================")
 		defintion := strings.SplitAfter(string(v), "#/definitions/")
 		fmt.Println(defintion[1])
 		//fmt.Printf( "The defintion %s \n", (swag.Definitions));
-		def, _, _, _ := jsonparser.Get(swag.Definitions, defintion[1])
+		def, _, _, _ := jsonparser.Get(Swag.Definitions, defintion[1])
+
+		ParseDefintions(defintion[1],def)
 		//fmt.Println(string(def))
-		var vardef Definitionsprops
-		_ = json.Unmarshal(def, &vardef)
-		vardef.name = defintion[1]
-		fmt.Println(vardef)
-		//v,_,_,_ = jsonparser.Get(swag.Definitions,defintion[1],"properties")
-		fmt.Println("================================")
 
-		for key, val := range vardef.Properties {
-			lname := key
-			/*var tmpProp property
-			_ = json.Unmarshal(val, &tmpProp )
-			tmpProp.Name = lname
-			fmt.Println("================================")
-
-			vardef.indprop = append(vardef.indprop, tmpProp)
-			fmt.Println("Name:",tmpProp.Name)
-			fmt.Println("Type:",tmpProp.Type)
-			fmt.Println("Format:",tmpProp.Format)
-			fmt.Println("Enum:",tmpProp.Enum)
-			fmt.Println("Items:",tmpProp.Items)
-			fmt.Println("Default:",tmpProp.Default)
-
-			fmt.Println("================================")*/
-
-
-			ltype := val.(map[string]interface{})["type"]
-			if ltype == nil {
-				ltype = ""
-			}
-
-			lFormat := val.(map[string]interface{})["format"]
-			if lFormat == nil {
-				lFormat = ""
-			}
-			lItems := val.(map[string]interface{})["items"]
-			if lItems == nil {
-				lItems = ""
-			}
-			lEnum := val.(map[string]interface{})["enum"]
-			if lEnum == nil {
-				lEnum = ""
-			}
-			lDefault := val.(map[string]interface{})["default"]
-			if lDefault == nil {
-				lDefault = false
-			}
-			lRefs := val.(map[string]interface{})["$ref"]
-			if lRefs == nil {
-				lRefs = ""
-			}
-			fmt.Println("================================")
-			fmt.Println("new property")
-			fmt.Println("Name:", lname)
-			fmt.Println("Type:", ltype)
-			fmt.Println("Format:", lFormat)
-			fmt.Println("Items:", lItems)
-			fmt.Println("Enum:", lEnum)
-			fmt.Println("Refs:", lRefs)
-			fmt.Println("Default:", lDefault)
-			fmt.Println("================================")
-
-			tmpProperty := property{Name:string(lname), Type:ltype.(string), Format:lFormat.(string), Items:lItems, Enum:lEnum,Refs:lRefs.(string), Default:lDefault.(bool)}
-
-			vardef.indprop = append(vardef.indprop, tmpProperty)
-
-		}
-		Defs = append(Defs,vardef)
 
 	}
 	fmt.Println("**************************************************************")
@@ -144,15 +251,16 @@ func main() {
 	fmt.Println("**************************************************************")
 
 	//var propertymap map[string]json.RawMessage
-	for iter := range Defs{
-		fmt.Println(Defs[iter].name)
-		fmt.Println(Defs[iter].Properties)
-		fmt.Println(Defs[iter].indprop)
+	for iter := range Swag.Defs{
+		fmt.Println(Swag.Defs[iter].Name)
+		fmt.Println(Swag.Defs[iter].Properties)
+		fmt.Println(Swag.Defs[iter].indprop)
 
 
 		fmt.Println("**************************************************************")
 
 	}
+	generateModel(Swag)
 	fmt.Print(err2)
 
 
